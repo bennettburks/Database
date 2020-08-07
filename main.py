03c21b30769b24820807c21540fdcb0b8a67d8fb
bennettburks <60748003+bennettburks@users.noreply.github.com>
2020-08-06 19:26:09 -0500

    Added JSON file reading
    File is now printed to screen

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-import tkinter
+#main file with intialization 
+from tkinter import *
+from os import path
+import json
 
-window = tkinter.Tk()
-window.mainloop()
+def main():
+    window = Tk()
+    window.title("Database")
+    window.geometry("800x600")
+
+    left_window = LabelFrame(window, text="File Contents")
+    left_window.pack(fill="both", expand="yes", padx=10, pady=10)
+
+    json_text = Text(left_window)
+    json_text.insert(INSERT, check_file())
+    json_text.pack()
+
+    window.mainloop()
+
+
+def check_file():
+    if not path.exists("data.json"):
+        json_file = open("data.json", "x")
+        json_file.close()
+    with open("data.json", "r") as json_file:
+        json_text = json.loads(json_file.read())
+        return str(json_text)
+
+
+if __name__ == "__main__":
+    main()
+
+
