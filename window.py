1804c420140d3b905df58f66b4117da874bfccf4
bennettburks <60748003+bennettburks@users.noreply.github.com>
2020-08-08 20:52:39 -0500

    Button now shows key values

--- window.py
+++ window.py
@@ -19,35 +19,43 @@ def window_init():
     left_window = LabelFrame(window, text="File Contents")
     left_window.pack(fill="both", expand="yes", padx=10, pady=10, side=LEFT)
 
-    json_text = Text(left_window)
-    json_text.insert(INSERT, check_file())
-    json_text.pack()
+    text = Text(left_window)
+    text.insert(INSERT, str(file_contents()))
+    text.pack()
 
     #right window init
     right_window = LabelFrame(window, text="Database Tags")
     right_window.pack(fill="both", expand="yes", padx=10, pady=10, side=RIGHT)
 
-    json_tags = Listbox(right_window)
+    tags = Listbox(right_window)
     for itm in get_tags():
-        json_tags.insert(END, itm)
-    json_tags.pack()
+        tags.insert(END, itm)
+    tags.pack()
     #lambda allows called function to have parameters
-    json_button = Button(right_window, text="Update Database", command=lambda: update_json_text(json_text))
+    json_button = Button(right_window, text="Update Database", command=lambda: update_text(text, tags))
     json_button.pack()
 
-def update_json_text(json_text):
-    json_text.delete("1.0", END)
-    json_text.insert(END, "test input")
+#updates left text box
+def update_text(text, tags):
+    data = file_contents()
+    text.delete("1.0", END)
+    text.insert(END, data[tags.get(ACTIVE)])
 
+#creates data file if it doesn't exist
 def check_file():
     if not path.exists("data.json"):
         json_file = open("data.json", "x")
         json_file.close()
-    with open("data.json", "r") as json_file:
-        json_text = json.loads(json_file.read())
-        return str(json_text)
 
+#returns data file contents (dictionary)
+def file_contents():
+    check_file()
+    with open("data.json", "r") as json_file:
+        text = json.loads(json_file.read())
+        return text
+
+#returns tags of dictionary from data file
 def get_tags():
     with open("data.json", "r") as json_file:
-        json_text = json.loads(json_file.read())
-        return json_text.keys()
+        text = json.loads(json_file.read())
+        return text.keys()
